1a747ca499c142a231054d49510b633ad68e5baf
Sophie Zhao <syzhao@andrew.cmu.edu>
2016-05-02 16:58:31 -0400

    shifts set up and authorize store

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,6 +2,8 @@ class ShiftsController < ApplicationController
   before_action :set_shift, only: [:show, :edit, :update, :destroy]
   
   def index
+    @upcoming_shifts  = Shift.upcoming.by_store.by_employee.chronological.paginate(page: params[:page]).per_page(15)
+    @past_shifts = Shift.past.by_store.by_employee.chronological.paginate(page: params[:page]).per_page(15)
   end
 
   def show
@@ -15,12 +17,25 @@ class ShiftsController < ApplicationController
   end
 
   def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+      redirect_to shift_path(@shift), notice: "Shift created for #{@shift.assignment.employee.proper_name} at #{@shift.assignment.store.name}."
+    else
+      render action: 'new'
+    end
   end
 
   def update
+    if @shift.update(shift_params)
+      redirect_to shift_params, notice: "#{@shift.assignment.employee.proper_name}'s shift at #{@shift.assignment.store.name} is updated."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @shift.destroy
+    redirect_to shifts_path, notice: "Successfully removed #{@shift.assignment.employee.proper_name}'s shift from #{@shift.assignment.store.name}."
   end
 
   private
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,25 +1,30 @@
 class StoresController < ApplicationController
   before_action :set_store, only: [:show, :edit, :update, :destroy]
-  authorize resource
+
   
   def index
     @active_stores = Store.active.alphabetical.paginate(page: params[:page]).per_page(10)
     @inactive_stores = Store.inactive.alphabetical.paginate(page: params[:page]).per_page(10)  
+
   end
 
   def show
     @current_assignments = @store.assignments.current.by_employee.paginate(page: params[:page]).per_page(8)
+    authorize! :show, @store
   end
 
   def new
     @store = Store.new
+    authorize! :new, @store
   end
 
   def edit
+    authorize! :edit, @store
   end
 
   def create
     @store = Store.new(store_params)
+    authorize! :create, @store
     if @store.save
       redirect_to store_path(@store), notice: "Successfully created #{@store.name}."
     else
@@ -28,6 +33,7 @@ class StoresController < ApplicationController
   end
 
   def update
+    authorize! :update, @store
     if @store.update(store_params)
       redirect_to store_path(@store), notice: "Successfully updated #{@store.name}."
     else
@@ -36,6 +42,7 @@ class StoresController < ApplicationController
   end
 
   def destroy
+    authorize! :destroy, @store
     @store.destroy
     redirect_to stores_path, notice: "Successfully removed #{@store.name} from the AMC system."
   end
