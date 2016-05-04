2c2073a72113be3c00b2684e2c199ac22b2509c8
Sophie Zhao <syzhao@andrew.cmu.edu>
2016-05-04 10:56:58 -0400

    last commit, colors changed!

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,7 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   
   def index
+    @jobs = Job.alphabetical
   end
 
   def show
@@ -9,27 +10,44 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
+    authorize! :new, @job
   end
 
   def edit
   end
 
   def create
+    @job = Job.new(job_params)
+    authorize! :create, @job
+    if @job.save
+      redirect_to job_path(@job), notice: "Successfully created #{@job.name}."
+    else
+      render action: 'new'
+    end
   end
 
   def update
+    authorize! :update, @job
+    if @job.update(job_params)
+      redirect_to job_path(@job), notice: "Successfully updated #{@job.name}."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    authorize! :destroy, @job
+    @job.destroy
+    redirect_to jobs_path, notice: "Successfully removed #{@job.name} from the AMC system."
   end
 
   private
   def set_job
-    @job = job.find(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def job_params
-    params.require(:job).permit(:first_name, :last_name, :ssn, :date_of_birth, :role, :phone, :active)
+    params.require(:job).permit(:name, :description)
   end
 
 end
